e0ac5b9a3c42470d6ead24868e94beb9904ff888
bohaos <bohaos@outlook.com>
2026-05-26 16:08:53 +0800

    feat: add proof

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -932,7 +932,9 @@ func (app *BaseApp) RunTx(proof *cmtproto.Proof, mode sdk.ExecMode, txBytes []by
 	// in case message processing fails. At this point, the MultiStore
 	// is a branch of a branch.
 	runMsgCtx, msCache := app.cacheTxContext(ctx)
-
+	if mode == execModeFinalize && proof != nil {
+		runMsgCtx = runMsgCtx.WithValue("proof", proof)
+	}
 	// Attempt to execute all messages and only update state if all messages pass
 	// and we're in DeliverTx. Note, runMsgs will never return a reference to a
 	// Result if any single message fails or does not have a registered Handler.
